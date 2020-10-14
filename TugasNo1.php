b329b4f1de481bbd94f0a2d5560f3c34727429ab
Ghulamrijal <ghulamrijal@gmail.com>
2020-10-14 15:51:40 +0700

    Fix

---
+++ TugasNo1.php
@@ -0,0 +1,14 @@
+<?php 
+    $nilai = "90";
+    if (($nilai >= 86 ) AND ($nilai <=100)) {
+        echo "SELAMAT.... proses belajarmu sangat baik, tingkatkan kemampuan codingnya :)";
+    } 
+    else if (($nilai >= 71) AND ($nilai <=85)) {
+        echo "Selamat… kamu sudah mulai memahami pemrgraman PHP, tingkatkan terus skil codingmu…";
+    }
+    elseif (($nilai >= 60) AND ($nilai <=70)) {
+        echo "uppsssss….. skil kodingmu perlu diasah ini gaes, tetap semangat  belajar dan eksplore kemampuan y…";
+
+    }
+    
+?>
---
+++ TugasNo2.php
@@ -0,0 +1,9 @@
+<?php
+for ($i= 1; $i <= 3; $i++)
+{
+   echo "Hallo Nama Saya Ghulam <br>";
+   echo "Saya Mahasiswa Prodi Sistem Informasi <br>";
+   echo "Saya Sangat Tertarik Dengan Seseorang :)";
+   echo "<hr>";
+}
+?>
